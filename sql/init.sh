0603e786594158851ad66d6b84143dce2ad37f7e
cyrill <1283323+t-cyrill@users.noreply.github.com>
2024-12-08 16:44:42 +0900

    初期データに合わせてカラムを追加

--- sql/init.sh
+++ sql/init.sh
@@ -35,3 +35,9 @@ gzip -dkc 3-initial-data.sql.gz | cat disable.slowlog.sql - | mysql -u"$ISUCON_D
 		--host "$ISUCON_DB_HOST" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME"
+
+mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_DB_HOST" \
+		--port "$ISUCON_DB_PORT" \
+		"$ISUCON_DB_NAME" < 4-after-data.sql
