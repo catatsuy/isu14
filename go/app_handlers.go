daa38242715de72cc3da2b77eabe692a8e48ab74
catatsuy <catatsuy@catatsuy.org>
2024-12-08 14:33:15 +0900

    Improve ride status handling and optimize query for ride statuses.

--- go/app_handlers.go
+++ go/app_handlers.go
@@ -774,36 +774,58 @@ func getChairStats(ctx context.Context, tx *sqlx.Tx, chairID string) (appGetNoti
 		return stats, err
 	}
 
+	if len(rides) == 0 {
+		// rides が空の場合はそのまま返す
+		return stats, nil
+	}
+
+	// ride IDs を抽出
+	rideIDs := make([]string, len(rides))
+	for i, ride := range rides {
+		rideIDs[i] = ride.ID
+	}
+
+	rideStatuses := []RideStatus{}
+	query, args, err := sqlx.In(
+		`SELECT * FROM ride_statuses WHERE ride_id IN (?) ORDER BY created_at`,
+		rideIDs,
+	)
+	if err != nil {
+		return stats, err
+	}
+	query = tx.Rebind(query)
+	err = tx.SelectContext(ctx, &rideStatuses, query, args...)
+	if err != nil {
+		return stats, err
+	}
+
+	rideStatusMap := make(map[string][]RideStatus)
+	for _, status := range rideStatuses {
+		rideStatusMap[status.RideID] = append(rideStatusMap[status.RideID], status)
+	}
+
 	totalRideCount := 0
 	totalEvaluation := 0.0
 	for _, ride := range rides {
-		rideStatuses := []RideStatus{}
-		err = tx.SelectContext(
-			ctx,
-			&rideStatuses,
-			`SELECT * FROM ride_statuses WHERE ride_id = ? ORDER BY created_at`,
-			ride.ID,
-		)
-		if err != nil {
-			return stats, err
+		statuses, ok := rideStatusMap[ride.ID]
+		if !ok {
+			continue
 		}
 
 		var arrivedAt, pickupedAt *time.Time
 		var isCompleted bool
-		for _, status := range rideStatuses {
-			if status.Status == "ARRIVED" {
+		for _, status := range statuses {
+			switch status.Status {
+			case "ARRIVED":
 				arrivedAt = &status.CreatedAt
-			} else if status.Status == "CARRYING" {
+			case "CARRYING":
 				pickupedAt = &status.CreatedAt
-			}
-			if status.Status == "COMPLETED" {
+			case "COMPLETED":
 				isCompleted = true
 			}
 		}
-		if arrivedAt == nil || pickupedAt == nil {
-			continue
-		}
-		if !isCompleted {
+
+		if arrivedAt == nil || pickupedAt == nil || !isCompleted || ride.Evaluation == nil {
 			continue
 		}
 
